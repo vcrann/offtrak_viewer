01c46abebc5b88b9d46685d0a9c7466798a10337
vcrann <verdon.crann@manchester.ac.uk>
2023-06-29 14:49:13 +0100

    added tracker manager

---
+++ lib/modules/core/managers/tracker_manager.dart
@@ -0,0 +1,283 @@
+//import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:latlng/latlng.dart';
+import 'dart:async';
+//import '../models/agent_state.dart';
+//import 'package:helixio_app/modules/helpers/service_locator.dart';
+
+
+class TrackerManager extends ChangeNotifier {
+  int swarmSize = 0;
+  LatLng _referencePoint = LatLng(53.43335012150398, -2.249079103930851);
+  var swarm = <String, AgentState>{}; // linter prefers this to map
+  List<String> selected = [];
+  bool anyArmed = false;
+  Map<String, List<String>> closePairs = {};
+  // void initialiseSwarm(int newSwarmSize, MQTTManager mqttManager) {
+  //   swarmSize = newSwarmSize;
+  //   swarm.clear();
+  //   for (int i = 0; i < newSwarmSize; i++) {
+  //     //Quick and hacky, only allows 9 drones and have to be sequential
+  //     //fix later!!
+  //     String id = 'P10' + (i + 1).toString();
+  //     swarm[id] = AgentState(id);
+  //   }
+  //   subscribeToSwarm(mqttManager);
+  // }
+
+  void setReferencePoint(LatLng referencePoint) {
+    _referencePoint = referencePoint;
+  }
+
+  void addSelected(String id) {
+    selected.add(id);
+  }
+
+  void removeSelected(String id) {
+    selected.remove(id);
+  }
+
+  void update() {
+    notifyListeners();
+  }
+
+  void subscribeToSwarm(MQTTManager mqttManager) {
+    //_mqttManager = Provider.of<MQTTManager>(context, listen: false);
+    for (String id in swarm.keys) {
+      // use wildcard to subscribe to all updates from each drone ID
+      mqttManager.subscribeTo(id + '/#');
+    }
+  }
+
+  void proximityPeriodicUpdate() {
+    const oneSec = Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer timer) {
+      if (!anyArmed) {
+        timer.cancel();
+      }
+      Map<String, NED> swarmPositionsNed = {};
+      swarm.forEach((agent, agentState) {
+        swarmPositionsNed[agent] = agentState.getNED;
+      });
+      checkProximity(swarm);
+    });
+  }
+
+  List<double> decodeTelemetry(String telemetry) {
+    //converts strings to doubles
+    var stringArray = telemetry.split(', ');
+
+    List<double> doubleArray = [];
+    for (int i = 0; i < stringArray.length; i++) {
+      doubleArray.add(double.parse(stringArray[i]));
+    }
+
+    return doubleArray;
+  }
+
+  // void handleMessage(String topic, ByteData payload) {
+  //   var topicArray = topic.split('/');
+  //   // check if the message is on the detection topic
+  //   if (topicArray[0] == 'detection') {
+  //     //add an agent to the swarm
+  //     if (!swarm.containsKey(payload)) {
+  //       swarm[payload] = AgentState(payload);
+  //       serviceLocator<MQTTManager>().subscribeTo(payload + '/#');
+  //     }
+  //   } else {
+  //     // if the top level topic isnt detection it must be a drone id
+  //     String id = topicArray[0];
+  //     switch (topicArray[1]) {
+  //       case 'connection_status':
+  //         swarm[id]?.setConnected(payload);
+  //         break;
+  //       case 'flight_mode':
+  //         swarm[id]?.setFlightMode(payload);
+  //         break;
+  //       case 'battery_level':
+  //         swarm[id]?.setBatteryLevel(int.parse(payload));
+  //         break;
+  //       case 'wifi_strength':
+  //         swarm[id]?.setWifiStrength(int.parse(payload));
+  //         break;
+  //       case 'telemetry':
+  //         switch (topicArray[2]) {
+  //           case 'geodetic':
+  //             // geodetic message needs to be decoded
+  //             var geodeticTelem = decodeTelemetry(payload);
+  //             swarm[id]?.setGeodetic(
+  //                 LatLng(geodeticTelem[0], geodeticTelem[1]), geodeticTelem[2]);
+  //             break;
+  //           case 'heading':
+  //             swarm[id]?.setHeading(double.parse(payload));
+  //             break;
+  //           case 'position_ned':
+  //             // position message needs to be decoded
+  //             var positionNED = decodeTelemetry(payload);
+  //             swarm[id]
+  //                 ?.setNED(NED(positionNED[0], positionNED[1], positionNED[2]));
+  //             break;
+  //           case 'arm_status':
+  //             bool armStatus = payload.toLowerCase() == 'true';
+  //             if (armStatus != swarm[id]?.getArmStatus) {
+  //               swarm[id]?.setArmStatus(armStatus);
+  //               if (armStatus) {
+  //                 anyArmed = true;
+  //                 proximityPeriodicUpdate();
+  //               } else {
+  //                 for (var agentState in swarm.values) {
+  //                   if (agentState.getArmStatus) {
+  //                     break;
+  //                   }
+  //                 }
+  //                 //stop proximity update as all agents are disarmed
+  //                 anyArmed = false;
+  //               }
+  //             }
+  //             break;
+  //         }
+  //     }
+  //     notifyListeners();
+  //   }
+  // }
+
+  LatLng get getReferencePoint => _referencePoint;
+}
+  int swarmSize = 0;
+  LatLng _referencePoint = LatLng(53.43335012150398, -2.249079103930851);
+  var swarm = <String, AgentState>{}; // linter prefers this to map
+  List<String> selected = [];
+  bool anyArmed = false;
+  Map<String, List<String>> closePairs = {};
+  // void initialiseSwarm(int newSwarmSize, MQTTManager mqttManager) {
+  //   swarmSize = newSwarmSize;
+  //   swarm.clear();
+  //   for (int i = 0; i < newSwarmSize; i++) {
+  //     //Quick and hacky, only allows 9 drones and have to be sequential
+  //     //fix later!!
+  //     String id = 'P10' + (i + 1).toString();
+  //     swarm[id] = AgentState(id);
+  //   }
+  //   subscribeToSwarm(mqttManager);
+  // }
+
+  void setReferencePoint(LatLng referencePoint) {
+    _referencePoint = referencePoint;
+  }
+
+  void addSelected(String id) {
+    selected.add(id);
+  }
+
+  void removeSelected(String id) {
+    selected.remove(id);
+  }
+
+  void update() {
+    notifyListeners();
+  }
+
+  void subscribeToSwarm(MQTTManager mqttManager) {
+    //_mqttManager = Provider.of<MQTTManager>(context, listen: false);
+    for (String id in swarm.keys) {
+      // use wildcard to subscribe to all updates from each drone ID
+      mqttManager.subscribeTo(id + '/#');
+    }
+  }
+
+  void proximityPeriodicUpdate() {
+    const oneSec = Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer timer) {
+      if (!anyArmed) {
+        timer.cancel();
+      }
+      Map<String, NED> swarmPositionsNed = {};
+      swarm.forEach((agent, agentState) {
+        swarmPositionsNed[agent] = agentState.getNED;
+      });
+      checkProximity(swarm);
+    });
+  }
+
+  List<double> decodeTelemetry(String telemetry) {
+    //converts strings to doubles
+    var stringArray = telemetry.split(', ');
+
+    List<double> doubleArray = [];
+    for (int i = 0; i < stringArray.length; i++) {
+      doubleArray.add(double.parse(stringArray[i]));
+    }
+
+    return doubleArray;
+  }
+
+  // void handleMessage(String topic, ByteData payload) {
+  //   var topicArray = topic.split('/');
+  //   // check if the message is on the detection topic
+  //   if (topicArray[0] == 'detection') {
+  //     //add an agent to the swarm
+  //     if (!swarm.containsKey(payload)) {
+  //       swarm[payload] = AgentState(payload);
+  //       serviceLocator<MQTTManager>().subscribeTo(payload + '/#');
+  //     }
+  //   } else {
+  //     // if the top level topic isnt detection it must be a drone id
+  //     String id = topicArray[0];
+  //     switch (topicArray[1]) {
+  //       case 'connection_status':
+  //         swarm[id]?.setConnected(payload);
+  //         break;
+  //       case 'flight_mode':
+  //         swarm[id]?.setFlightMode(payload);
+  //         break;
+  //       case 'battery_level':
+  //         swarm[id]?.setBatteryLevel(int.parse(payload));
+  //         break;
+  //       case 'wifi_strength':
+  //         swarm[id]?.setWifiStrength(int.parse(payload));
+  //         break;
+  //       case 'telemetry':
+  //         switch (topicArray[2]) {
+  //           case 'geodetic':
+  //             // geodetic message needs to be decoded
+  //             var geodeticTelem = decodeTelemetry(payload);
+  //             swarm[id]?.setGeodetic(
+  //                 LatLng(geodeticTelem[0], geodeticTelem[1]), geodeticTelem[2]);
+  //             break;
+  //           case 'heading':
+  //             swarm[id]?.setHeading(double.parse(payload));
+  //             break;
+  //           case 'position_ned':
+  //             // position message needs to be decoded
+  //             var positionNED = decodeTelemetry(payload);
+  //             swarm[id]
+  //                 ?.setNED(NED(positionNED[0], positionNED[1], positionNED[2]));
+  //             break;
+  //           case 'arm_status':
+  //             bool armStatus = payload.toLowerCase() == 'true';
+  //             if (armStatus != swarm[id]?.getArmStatus) {
+  //               swarm[id]?.setArmStatus(armStatus);
+  //               if (armStatus) {
+  //                 anyArmed = true;
+  //                 proximityPeriodicUpdate();
+  //               } else {
+  //                 for (var agentState in swarm.values) {
+  //                   if (agentState.getArmStatus) {
+  //                     break;
+  //                   }
+  //                 }
+  //                 //stop proximity update as all agents are disarmed
+  //                 anyArmed = false;
+  //               }
+  //             }
+  //             break;
+  //         }
+  //     }
+  //     notifyListeners();
+  //   }
+  // }
+
+  LatLng get getReferencePoint => _referencePoint;
+}
---
+++ lib/modules/core/widgets/map_view.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/material.dart';
+//import 'package:helixio_app/pages/page_scaffold.dart';
+import 'package:offtrak_viewer/modules/core/secrets/keys.dart';
+//import 'package:helixio_app/modules/helpers/service_locator.dart';
+//import 'package:helixio_app/modules/core/models/agent_state.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/gestures.dart';
+import 'package:latlng/latlng.dart';
+import 'package:map/map.dart';
+import 'package:timer_builder/timer_builder.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:math';
+
+class MapView extends StatefulWidget {
+  const MapView({Key? key}) : super(key: key);
+  //final SwarmManager swarmManager;
+
+  @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  final controller = MapController(
+    //location: LatLng(53.43578053111544, -2.250343561172483),
+    //location: LatLng(52.81651946850575, -4.124781265539541),
+    location: const LatLng(53.407712727422705, -2.12315514168248),
+  );
+
+  bool _darkMode = false;
+
+  // List<AgentMarker> _generateMarkers(var swarm, MapTransformer transformer) {
+  //   List<AgentMarker> agentMarkers = [];
+
+  //   if (swarm.isNotEmpty) {
+  //     Iterable<AgentState> agents = swarm.values;
+  //     for (AgentState agent in agents) {
+  //       agentMarkers.add(AgentMarker(agent.getLatLng, agent.getHeading,
+  //           transformer.toOffset(agent.getLatLng)));
+  //     }
+  //   }
+  //   return agentMarkers;
+  // }
+
+  void _gotoDefault() {
+    controller.center = const LatLng(53.43578053111544, -2.250343561172483);
+    setState(() {});
+  }
+
+  Future<void> _getUserLocation() async {
+    print("getting location");
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    controller.center = LatLng(position.latitude, position.longitude);
+    print("latitude= " + position.latitude.toString());
+    print("longitude= " + position.longitude.toString());
+    setState(() {});
+  }
+
+  void _onDoubleTap() {
+    controller.zoom += 0.5;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller.zoom = 16.5;
+    setState(() {});
+  }
+
+  // void _setInitialZoom() {
+  //   controller.zoom = 10;
+  //   setState(() {});
+  // }
+
+  Offset? _dragStart;
+  double _scaleStart = 1.0;
+  void _onScaleStart(ScaleStartDetails details) {
+    _dragStart = details.focalPoint;
+    _scaleStart = 1.0;
+  }
+
+  void _onScaleUpdate(ScaleUpdateDetails details, MapTransformer transformer) {
+    final scaleDiff = details.scale - _scaleStart;
+    _scaleStart = details.scale;
+
+    if (scaleDiff > 0) {
+      controller.zoom += 0.02;
+      setState(() {});
+    } else if (scaleDiff < 0) {
+      controller.zoom -= 0.02;
+      setState(() {});
+    } else {
+      final now = details.focalPoint;
+      final diff = now - _dragStart!;
+      _dragStart = now;
+      transformer.drag(diff.dx, diff.dy);
+      setState(() {});
+    }
+  }
+
+  Widget _buildMarkerWidget(Offset pos, double heading, Color color) {
+    return Positioned(
+      left: pos.dx - 16,
+      top: pos.dy - 16,
+      width: 24,
+      height: 24,
+      // child: Row(
+      //   children: [
+      //     const Text('SXXX'),
+      //     Icon(Icons.airplanemode_active, color: color),
+      //   ],
+      // ),
+      child: Transform.rotate(
+          //convert heading in degrees to radians
+          angle: heading * (pi / 180),
+          child: Icon(Icons.airplanemode_active, color: color, size: 15)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('drawing map');
+
+    return Stack(children: [
+      MapLayout(
+        controller: controller,
+        builder: (context, transformer) {
+          final centerLocation = Offset(
+              transformer.constraints.biggest.width / 2,
+              transformer.constraints.biggest.height / 2);
+
+          return GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onDoubleTap: _onDoubleTap,
+            onScaleStart: _onScaleStart,
+            onScaleUpdate: (details) => _onScaleUpdate(details, transformer),
+            onTapUp: (details) {
+              final location = transformer.toLatLng(details.localPosition);
+
+              final clicked = transformer.toOffset(location);
+
+              print('${location.longitude}, ${location.latitude}');
+              print('${clicked.dx}, ${clicked.dy}');
+              print('${details.localPosition.dx}, ${details.localPosition.dy}');
+            },
+            child: Listener(
+              behavior: HitTestBehavior.opaque,
+              onPointerSignal: (event) {
+                if (event is PointerScrollEvent) {
+                  final delta = event.scrollDelta;
+
+                  controller.zoom -= delta.dy / 1000.0;
+                  setState(() {});
+                }
+              },
+              child: Stack(
+                children: [
+                  TileLayer(
+                    builder: (context, x, y, z) {
+                      final tilesInZoom = pow(2.0, z).floor();
+
+                      while (x < 0) {
+                        x += tilesInZoom;
+                      }
+                      while (y < 0) {
+                        y += tilesInZoom;
+                      }
+
+                      x %= tilesInZoom;
+                      y %= tilesInZoom;
+                      //Mapbox Streets
+                      final url =
+                          'https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/$z/$x/$y?access_token=${mapBoxApiKey}';
+
+                      return CachedNetworkImage(
+                        imageUrl: url,
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  ),
+                  // used to update markers at set frequency rather than as fast as updates are received
+                  // increases performance
+                  TimerBuilder.periodic(const Duration(milliseconds: 1000),
+                      builder: (context) {
+                    print("rebuilding markers");
+                    var swarm = serviceLocator<SwarmManager>().swarm;
+
+                    final markerPositions =
+                        _generateMarkers(swarm, transformer);
+
+                    final markerWidgets = markerPositions.map(
+                      (agentMarker) => _buildMarkerWidget(agentMarker.cartesian,
+                          agentMarker.heading, Colors.red),
+                    );
+                    return Stack(
+                      children: [...markerWidgets],
+                    );
+                  }),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+      Align(
+        alignment: Alignment.bottomRight,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Ink(
+            decoration: const ShapeDecoration(
+              color: Colors.lightBlue,
+              shape: CircleBorder(),
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.my_location),
+              color: Colors.white,
+              onPressed: () {
+                _getUserLocation();
+              },
+            ),
+          ),
+        ),
+      ),
+    ]);
+  }
+}
+
+// Defines marker object containing position and heading
+class AgentMarker {
+  final LatLng latLng;
+  final double heading;
+  final Offset cartesian;
+
+  AgentMarker(this.latLng, this.heading, this.cartesian);
+
+  // LatLng get getLatLng => _latLng;
+  // double get getHeading => _heading;
+  // Offset get getCartesian => _cartesian;
+}
